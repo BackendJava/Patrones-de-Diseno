4be01672aadc5c1dbdb461fc0296b417ed472b38
elmergustavo <elmergustavo79@gmail.com>
2021-06-15 10:13:21 -0600

    decorator

--- Strategy/src/main/java/com/main/App.java
+++ Strategy/src/main/java/com/main/App.java
@@ -1,12 +1,12 @@
 package com.main;
 
-import com.strategy.AntivirusAvanzado;
+import com.strategy.AntivirusSimple;
 import com.strategy.Contexto;
 
 public class App {
 	
 	public static void main(String[] args) {
-		Contexto contexto = new Contexto(new AntivirusAvanzado());
+		Contexto contexto = new Contexto(new AntivirusSimple());
 		contexto.ejecutar();
 	}
 
