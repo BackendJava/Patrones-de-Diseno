509401d6c0011041e7e456c646dd80869c49661c
elmergustavo <elmergustavo79@gmail.com>
2021-07-04 08:09:23 -0600

    nuevo libro en spanish

--- Observer/src/main/java/com/url/App.java
+++ Observer/src/main/java/com/url/App.java
@@ -11,8 +11,7 @@ public class App {
 		Subject subject = new Subject();
 
 		new SolObservador(subject);
-		new PesoARGObservador(subject);
-		new PesoMXObservador(subject);
+		
 		
 		System.out.println("Si desea cambiar 10 dólares obtendrá : ");
 		subject.setEstado(10);
