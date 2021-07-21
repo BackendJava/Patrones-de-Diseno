c1b138b4c226f004e56ea6444f7c46c8ac5cb4fa
elmergustavo <elmergustavo79@gmail.com>
2021-07-21 11:57:16 -0600

    libro patters

--- Strategy/src/main/java/com/main/App.java
+++ Strategy/src/main/java/com/main/App.java
@@ -1,13 +1,14 @@
 package com.main;
 
+import com.strategy.AntivirusAvanzado;
 import com.strategy.AntivirusSimple;
 import com.strategy.Contexto;
 
 public class App {
-	
-	public static void main(String[] args) {
-		Contexto contexto = new Contexto(new AntivirusSimple());
-		contexto.ejecutar();
-	}
+
+    public static void main(String[] args) {
+        Contexto contexto = new Contexto(new AntivirusAvanzado());
+        contexto.ejecutar();
+    }
 
 }
